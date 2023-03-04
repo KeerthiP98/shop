ee4e3d17f84fb2fa565bfcc36ca8cd5aca803817
KeerthiP98 <126953047+KeerthiP98@users.noreply.github.com>
2023-03-04 10:27:48 +0000

    Update App.js

--- shop/client/src/App.js
+++ shop/client/src/App.js
@@ -1,9 +1,6 @@
 import { BrowserRouter as Router,Routes,Route } from "react-router-dom";
 import NavBar from "./components/nav/navBar";
 import Home from "./components/pages/Home";
-import Signin from "./components/pages/sign_in";
-import Login from "./components/pages/login";
-import List from "./components/pages/list"
 import {Provider} from "react-redux";
 import {store} from "./redux/store";
 function App() {
@@ -16,9 +13,7 @@ function App() {
     <Routes>
 
       <Route exact path="/" element={<Home />} />
-      <Route exact path="/signin" element={<Signin />} />
-      <Route exact path="/login" element={<Login />} />
-      <Route exact path="/list" element={<List />} />
+      
 
 
 
